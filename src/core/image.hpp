7400c8916f5bf17e3d42479e44112d85afb926ff
ssugary <ssugary0@gmail.com>
2026-04-14 12:18:20 -0300

    refactor: pequena alteração nos parâmetros das funções de exportar imagem e implementação da lógida das mesmas

--- src/core/image.hpp
+++ src/core/image.hpp
@@ -5,16 +5,21 @@
 // ===
 # include <cstdint>
 # include <string>
+# include <vector>
+
+# include "common.hpp"
+# include "lodepng/lodepng.h"
+
 
 namespace rt {
 /// Routines to write images to a file.
-bool save_ppm6(const std::uint8_t*, size_t, size_t, size_t = 1, const std::string& = "image.ppm");
+bool save_ppm6(const std::vector<RGBColor>, size_t, size_t, const std::string& = "image.ppm", size_t = 1);
 
 /// Saves an image as a **ascii** PPM file.
-bool save_ppm3(const std::uint8_t*, size_t, size_t, size_t = 1, const std::string& = "image.ppm");
+bool save_ppm3(const std::vector<RGBColor>, size_t, size_t, const std::string& = "image.ppm", size_t = 1);
 
 /// Saves an image as a PNG file.
-bool save_png(const std::uint8_t*, size_t, size_t, size_t = 1, const std::string& = "image.png");
+bool save_png(const std::vector<RGBColor>, size_t, size_t, const std::string& = "image.png", size_t = 1);
 }  // namespace rt
 // ===
 #endif
