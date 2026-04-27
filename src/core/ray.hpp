9b144d493109d3b1db76ac3235ee253c8d2725b5
ssugary <ssugary0@gmail.com>
2026-04-27 19:52:12 -0300

    refactor: getters e setters constantes e o operador () está público agora.

--- src/core/ray.hpp
+++ src/core/ray.hpp
@@ -11,30 +11,32 @@ using Vector3 = vec3<double>;
 class Ray {
     private:
 
-    Point3 position{};
+    Point3 origin{};
     Vector3 direction{};
 
     mutable double t_min{}, t_max{};    
-
-    constexpr Point3 operator()(double t) const {return position + direction * t;}
     
 
     public:
 
         Ray() : t_min(0.0), t_max(INFINITY){};
 
-        Ray( Point3 pos, Vector3 dir, double t_min = 0.0, double t_max = INFINITY) : position(pos), direction(dir), t_min(t_min), t_max(t_max) {};
+        Ray( Point3 o, Vector3 dir, double t_min = 0.0, double t_max = INFINITY) : origin(o), direction(dir), t_min(t_min), t_max(t_max) {};
 
         void normalize(){direction.mk_unit_vec();};
         
-        void setPosition(Point3 pos){this->position = pos;};
+        void setOrigin(Point3 o){this->origin = o;};
         void setDirection(Vector3 dir){this->direction = dir;};
+        void setTmin(double t_min)const{this->t_min = t_min;}
+        void setTMax(double t_max)const{this->t_max = t_max;}
         
-        Point3 getPosition(){return this->position;};
-        Vector3 getDirection(){return this->direction;};
+        Point3 getOrigin()const{return this->origin;};
+        Vector3 getDirection()const{return this->direction;};
+        double getTMin()const{return this->t_min;}
+        double getTMax()const{return this->t_max;}
 
-
-};
+        constexpr Point3 operator()(double t) const {return origin + direction * t;}
+    };
 
 
 
