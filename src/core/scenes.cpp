6b2a7a0b822747146bdd6837e6c83bfeac8fe69a
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2026-04-30 00:20:53 -0300

    minor: adds namespace

--- src/core/scenes.cpp
+++ src/core/scenes.cpp
@@ -3,66 +3,71 @@
 #include "ssmath/vec3.hpp"
 #include <memory>
 
+namespace rt {
 
-Sphere::Sphere(Point3 center, float radius, Material& mat) : center(center), radius(radius){
-    material = std::shared_ptr<Material>(new Material(mat));
+Sphere::Sphere(Point3 center, float radius, Material &mat)
+    : center(center), radius(radius) {
+  material = std::shared_ptr<Material>(new Material(mat));
 }
 
-bool Sphere::intersect(const Ray& r, Surfel *sf) const {
-    Point3 oc = r.getOrigin() - center;
-    Vec3 v = r.getDirection();
+bool Sphere::intersect(const Ray &r, Surfel *sf) const {
+  Point3 oc = r.getOrigin() - center;
+  Vec3 v = r.getDirection();
 
-    double A = dot(v, v);
-    double hB = dot(oc, v);
-    double C = dot(oc, oc) - radius * radius;
+  double A = dot(v, v);
+  double hB = dot(oc, v);
+  double C = dot(oc, oc) - radius * radius;
 
-    double delta = hB * hB - A * C;
+  double delta = hB * hB - A * C;
 
-    if(delta < 0)return false;
+  if (delta < 0)
+    return false;
 
-    double sqr = std::sqrt(delta);
-    double root = (-hB - sqr) /A ;
+  double sqr = std::sqrt(delta);
+  double root = (-hB - sqr) / A;
 
-    if(root < r.getTMin() || root > r.getTMax()){
-        root = (-hB + sqr) / A;
-        if(root < r.getTMin() || root > r.getTMax()){
-            return false;
-        }
+  if (root < r.getTMin() || root > r.getTMax()) {
+    root = (-hB + sqr) / A;
+    if (root < r.getTMin() || root > r.getTMax()) {
+      return false;
     }
+  }
 
-    if(sf){
-        sf->time = root;
-        sf->p = r(root);
-        sf->n = (sf->p - center)/radius;
-        sf->wo = -r.getDirection();
-        sf->primitive = this;
-    }
-    r.setTMax(root);
+  if (sf) {
+    sf->time = root;
+    sf->p = r(root);
+    sf->n = (sf->p - center) / radius;
+    sf->wo = -r.getDirection();
+    sf->primitive = this;
+  }
+  r.setTMax(root);
 
-    return true;
+  return true;
 }
 
+bool Sphere::intersect_p(const Ray &r) const {
+  Point3 oc = r.getOrigin() - center;
+  Vec3 v = r.getDirection();
 
-bool Sphere::intersect_p(const Ray& r) const {
-    Point3 oc = r.getOrigin() - center;
-    Vec3 v = r.getDirection();
+  double A = dot(v, v);
+  double hB = dot(oc, v);
+  double C = dot(oc, oc) - radius * radius;
 
-    double A = dot(v, v);
-    double hB = dot(oc, v);
-    double C = dot(oc, oc) - radius * radius;
+  double delta = hB * hB - A * C;
 
-    double delta = hB * hB - A * C;
+  if (delta < 0)
+    return false;
 
-    if(delta < 0)return false;
+  double sqr = std::sqrt(delta);
+  double root = (-hB - sqr) / A;
 
-    double sqr = std::sqrt(delta);
-    double root = (-hB - sqr) /A ;
-
-    if(root < r.getTMin() || root > r.getTMax()){
-        root = (-hB + sqr) / A;
-        if(root < r.getTMin() || root > r.getTMax()){
-            return false;
-        }
+  if (root < r.getTMin() || root > r.getTMax()) {
+    root = (-hB + sqr) / A;
+    if (root < r.getTMin() || root > r.getTMax()) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
+
+} // namespace rt
--- src/core/scenes.hpp
+++ src/core/scenes.hpp
@@ -13,61 +13,59 @@ using Point3 = vec3<double>;
 using Point4 = vec4<double>;
 using Vec3 = vec3<double>;
 
-class Primitive; 
+namespace rt {
+
+class Primitive;
 struct Material;
 
-struct Surfel{
-	public:
-		Surfel( const Point3&p,const Vec3&n, const Vec3&wo, float time,
-			const Point2& uv, const Primitive *pri )
-			: p{p}, n{n}, wo{wo}, time{time}, uv{uv}, primitive{pri}
-            {/* empty */};
-            
-        Surfel() = default;
+struct Surfel {
+public:
+  Surfel(const Point3 &p, const Vec3 &n, const Vec3 &wo, float time,
+         const Point2 &uv, const Primitive *pri)
+      : p{p}, n{n}, wo{wo}, time{time}, uv{uv}, primitive{pri} { /* empty */ };
 
-		Point3 p;        //!< Contact point.
-		Vec3 n;       //!< The surface normal.
-		Vec3 wo;      //!< Outgoing direction of light, which is -ray.
-		double time;
-        Point2 uv;     //!< Parametric coordinate (u,v) of the hit surface.
-		const Primitive *primitive=nullptr; //!< Pointer to the primitive.
+  Surfel() = default;
+
+  Point3 p; //!< Contact point.
+  Vec3 n;   //!< The surface normal.
+  Vec3 wo;  //!< Outgoing direction of light, which is -ray.
+  double time;
+  Point2 uv; //!< Parametric coordinate (u,v) of the hit surface.
+  const Primitive *primitive = nullptr; //!< Pointer to the primitive.
 };
 
-
-
-struct Material {    
-    Material(rt::RGBColor color, std::string type) : color(color), type(type) {};
-    Material(Material& mat) : color(mat.color), type(mat.type) {};
-    rt::RGBColor color;
-    std::string type;
+struct Material {
+  Material(rt::RGBColor color, std::string type) : color(color), type(type) {};
+  Material(Material &mat) : color(mat.color), type(mat.type) {};
+  rt::RGBColor color;
+  std::string type;
 };
 
 class Primitive {
-    protected:
-        std::shared_ptr<Material> material;
-    public:
-        virtual ~Primitive() = default;
-        virtual bool intersect(const Ray& r, Surfel *sf) const = 0;
-        virtual bool intersect_p(const Ray& r) const = 0;
-        virtual const std::shared_ptr<Material> get_material() const {return material;}
+protected:
+  std::shared_ptr<Material> material;
 
+public:
+  virtual ~Primitive() = default;
+  virtual bool intersect(const Ray &r, Surfel *sf) const = 0;
+  virtual bool intersect_p(const Ray &r) const = 0;
+  virtual const std::shared_ptr<Material> get_material() const {
+    return material;
+  }
 };
-
 
 class Sphere : public Primitive {
-    private:
-        Point3 center{};
-        double radius{0};
-    public:
-        Sphere(Point3 center, float radius, Material& mat);
-        bool intersect(const Ray& r, Surfel *sf) const;
-        bool intersect_p(const Ray& r)const;
-    
+private:
+  Point3 center{};
+  double radius{0};
+
+public:
+  Sphere(Point3 center, float radius, Material &mat);
+  bool intersect(const Ray &r, Surfel *sf) const;
+  bool intersect_p(const Ray &r) const;
 };
 
+class Scene {};
 
-class Scene {
-
-};
-
+} // namespace rt
 #endif
