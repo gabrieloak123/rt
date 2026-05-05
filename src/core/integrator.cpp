1d7df7b16896b875053ba92e92b4cf2ae3d46b8d
ssugary <ssugary0@gmail.com>
2026-05-05 14:11:01 -0300

    fix: erro de sintaxe e prevenção de segmentation faults

--- src/core/integrator.cpp
+++ src/core/integrator.cpp
@@ -1,4 +1,5 @@
 #include "integrator.hpp"
+#include "background.hpp"
 #include "camera.hpp"
 #include "common.hpp"
 #include <optional>
@@ -14,9 +15,15 @@ std::optional<rt::RGBColor> RayCastIntegrator::Li(const Ray& ray, const rt::Scen
     }
     // Some form of determining the incoming radiance at the ray's origin.
     // Polymorphism in action.
-    auto fm = dynamic_pointer_cast<rt::FlatMaterial>( isect.primitive->get_material() );
+    auto fm = std::dynamic_pointer_cast<rt::FlatMaterial>( isect.primitive->get_material() );
     // Assign diffuse color to L.
-    L = fm->get_color(); // Call a method present only in FlatMaterial.
+
+    if(fm){
+        L = fm->get_color();
+    }
+    else{
+        L = rt::RGBColor(255, 0, 255);
+    }
     return L;
 
 }
@@ -26,20 +33,24 @@ void SamplerIntegrator::render(const rt::Scene& scene){
   // -------------------------------------------------------------
   // The Film object holds the memory for the image.
 
-  auto film = camera->film.get();
-  auto w = film->width();
-  auto h = film->height();
+  if(!camera){
+    return;
+  }
+  auto w = camera->film->width();
+  auto h = camera->film->height();
+
   // -------------------------------------------------------------
   // Traverse all pixels to shoot rays from.
-  for (int j = h - 1; j > 0; j--) {
+  for (int j = 0; j < h; j++) {
     for (int i = 0; i < w; i++) {
-        double u{float(i)/float(w)};
-        double v{float(j)/float(h) };
         Ray ray = camera->generate_ray(i, j);
-
                 // Determine the incoming light.
         auto temp_L =  Li( ray, scene );
+
+        auto u = float(i) / float(w);
+        auto v = 1.0f - float(j) / float(h);
         rt::RGBColor L = (temp_L.has_value()) ?  temp_L.value() : scene.background->sample(u, v);
+
         camera->film->add(rt::Pixel{static_cast<byte>(i), static_cast<byte>(j)}, L);
       }
   }
