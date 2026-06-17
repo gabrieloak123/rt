ce4afab67bab02a2aecf8a5206033423b8288b5c
ssugary <ssugary0@gmail.com>
2026-06-17 08:52:49 -0300

    feat: add Transform class

---
+++ src/core/transform.hpp
@@ -0,0 +1,31 @@
+#ifndef TRANSFORM_HPP
+#define TRANSFORM_HPP
+
+#include "camera.hpp"
+namespace rt {
+
+    class Transform {
+        private:
+            Mat4 t_matrix;     //< Transformation Matrix
+            Mat4 t_matrix_inv; //< Inverse Transformation Matrix
+        public:
+            Transform() : t_matrix(), t_matrix_inv() {};
+            Transform(const Mat4& t_matrix) : t_matrix(t_matrix), t_matrix_inv(t_matrix.inverse()) {};
+            Transform(const Mat4& t_matrix, const Mat4& t_matrix_inv) : t_matrix(t_matrix), t_matrix_inv(t_matrix_inv) {};
+
+            Transform inverse()   {return   Transform(t_matrix_inv, t_matrix);}
+            Transform transpose() {return Transform(t_matrix.transpose(), t_matrix_inv.transpose());}
+
+            Transform translate(const Vec3& delta);
+            Transform rotate(const double& angle, const Vec3& delta);
+            Transform scale(const Vec3& scales);
+            Transform lookAt(const Vec3& look_from, const Vec3& look_at, const Vec3& vup);
+
+            Mat4 getTMat()    const {return this->t_matrix;};
+            Mat4 getTMatInv() const {return this->t_matrix_inv;};
+    };
+
+}; //< namespace rt
+
+
+#endif
